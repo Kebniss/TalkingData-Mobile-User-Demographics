107d5ab5e3e9ca9d967e50db3abe9fc50b0a85f7
Ludovica Gonella <ludovica.gonella@gmail.com>
2016-08-06 18:21:12 -0700

    Commit with starting code

---
+++ src/talkingdata.py
@@ -0,0 +1,9 @@
+"""This is the capstone project for the Udacity Nanodegree in Machine Learning
+"""
+import pandas as pd
+import numpy as np
+
+data = pd.read_csv("data/gender_age_train.csv")
+
+type(data)
+data.describe
