2b50662378bc4945eedbcf4b8af53b786e46e92a
Ludovica <ludovica.gonella@gmail.com>
2016-08-16 12:30:02 -0700

    drafted some ideas in separate training set

--- src/separate training set.py
+++ src/separate training set.py
@@ -1,11 +1,24 @@
 """ This script loads the original training data and separates them in instances
 and labels and saves them in two separate files"""
 import pandas as pd
+import pickle as pkl
 
 data = pd.read_csv("data/gender_age_train.csv")
 labels = pd.DataFrame(data['group'])
 labels.columns = ['labels']
 instances = data.drop('group', axis=1)
 
+# converting all features to numbers to be compatible with numpy
+instances['gender'], mapping_gender = pd.factorize(instances['gender'])
+mapping_gender
+instances['device_id'], mapping_dev_id = pd.factorize(instances['device_id'])
+mapping_dev_id
+
 labels.to_csv('./data/train_labels.csv', index=False)
 instances.to_csv('./data/train_instances.csv', index=False)
+with open('data/factorize_mappings.pkl', 'w') as f:
+    pkl.dump([mapping_gender, mapping_dev_id], f)
+
+
+test = pd.read_csv("data/gender_age_test.csv")
+test.head()
