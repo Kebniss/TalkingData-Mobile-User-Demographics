dcaaadaf0ab2b9b9b718c10423b190971eb80cca
Ludovica Gonella <ludovica.gonella@gmail.com>
2016-08-19 12:37:30 -0700

    Comparing inner and left merge

--- src/separate training set.py
+++ src/separate training set.py
@@ -1,5 +1,5 @@
-""" This script loads the original training data and separates them in instances
-and labels and saves them in two separate files"""
+""" This script loads the original training data and separates them in
+instances and labels and saves them in two separate files"""
 import pandas as pd
 import pickle as pkl
 
@@ -14,12 +14,52 @@ app_events = pd.read_csv("data/app_events.csv")
 label_categories = pd.read_csv("data/label_categories.csv")
 events = pd.read_csv("data/events.csv")
 
-# join all the datasets to create the raw training datasets
-data = pd.merge(instances, events, how='left', on='device_id')
-data = data.merge(phone_info, how='left', on='device_id')
-data = data.merge(app_events, how='left', on='event_id')
-data = data.merge(app_info, how='left', on='app_id')
-data = data.merge(label_categories, how='left', on='label_id')
+# check if there are nan values in the datasets
+datasets = [instances, phone_info, events, app_info, app_events,
+            label_categories]
+names = ['instances', 'phone_info', 'events', 'app_info', 'app_events',
+         'label_categories']
+nans = {}
+for i, dataset in enumerate(datasets):
+    cols = dataset.columns
+    for _, col in enumerate(cols):
+        nulls = dataset[dataset[col].isnull()]
+        if nulls.empty:
+            print 'No NaNs found.'
+        else:
+            print 'Found NaN values: '
+            print nulls
+            nans[(names[i], col)] = nulls
+print nans
+len(label_cate)
+
+# join LEFT all the datasets to create the raw training datasets
+data_l = pd.merge(instances, events, how='left', on='device_id')
+data_l = data_l.merge(phone_info, how='left', on='device_id')
+data_l = data_l.merge(app_events, how='left', on='event_id')
+data_l = data_l.merge(app_info, how='left', on='app_id')
+data_l = data_l.merge(label_categories, how='left', on='label_id')
+
+# join all the datasets to find their INTERSECTION
+data_i = pd.merge(instances, events, how='inner', on='device_id')
+data_i = data_i.merge(phone_info, how='inner', on='device_id')
+data_i = data_i.merge(app_events, how='inner', on='event_id')
+data_i = data_i.merge(app_info, how='inner', on='app_id')
+data_i = data_i.merge(label_categories, how='inner', on='label_id')
+
+len(data_l) - len(data_i)
+
+# compare the two joins
+print "Amount of data when left joined: ", data_l.shape
+print "Amount of data when inner joined: ", data_i.shape
+print "data_l.head():"
+print data_l.head()
+print "data_l.head():"
+print data_i.head()
+print "data_l.describe()"
+print data_l.describe()
+print "data_i.describe()"
+print data_i.describe()
 
 # converting all features to numbers to be compatible with numpy
 data['gender'], map_gender = pd.factorize(data['gender'])
