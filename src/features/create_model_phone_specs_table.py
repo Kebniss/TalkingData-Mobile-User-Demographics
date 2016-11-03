1ad05ff1d8d166a16975c29ea8ab3183b3bd627a
Ludovica Gonella <ludovica.gonella@gmail.com>
2016-11-02 19:23:28 -0700

    updated an old file process to read files

--- src/features/create_model_phone_specs_table.py
+++ src/features/create_model_phone_specs_table.py
@@ -7,11 +7,11 @@ from dotenv import load_dotenv, find_dotenv
 dotenv_path = find_dotenv()
 load_dotenv(dotenv_path)
 RAW_DATA_DIR = os.environ.get("RAW_DATA_DIR")
+FEATURES_DATA_DIR = os.environ.get("FEATURES_DIR")
 
 data = pd.read_csv(os.path.join(RAW_DATA_DIR, 'Mod_phone_brand_device_model.csv')
 
 specs_table = data.drop_duplicates(['phone_brand', 'device_model'])
 specs_table = specs_table.drop('device_id', 1)
 
-path = os.getcwd() + '/data/processed/specs_table.csv'
-specs_table.to_csv(path, index=False)
+specs_table.to_csv(os.path.join(RAW_DATA_DIR, 'specs_table.csv'), index=False)
